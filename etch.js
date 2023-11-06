1a3a42535d7d42347ff5ba8b9929f236751eecea
dans <dansdana94@gmail.com>
2023-11-06 19:28:37 +0100

    reworked the 'grid size' function

--- etch.js
+++ etch.js
@@ -1,31 +1,36 @@
+const colors = ['red','blue','green','brown','yellow','pink','purple']
 
 let divs = document.querySelector('#divs')
 let usBtn = document.querySelector('#usBtn')
 let usInput = document.querySelector('#usInput')
 let inputDiv = document.querySelector('#inputDiv')
+let rainbowBtn = document.querySelector('#rainbowBtn')
+let darkBtn = document.querySelector('#darkBtn');
 let errorMsg = document.getElementById('errorMsg');
-const colors = ['red','blue','green','brown','yellow','pink','purple']
+
+let div = document.createElement('div');
+div.classList.add('di')
 
 function gridSize(n) {
-for(let i = 0; i<n; i++){
-    for(let j = 0; j<n; j++){
-        let div = document.createElement('div');
-        div.classList.add('di')
-        div.style.cssText=`width:calc(100%/${n});`
-
-    if (n >=1 && n <= 100){
-        divs.appendChild(div)}
-
-    else {errorMsg.innerText=('value must be less >= 100')}
-
-    div.addEventListener('mousemove',()=> {div.style.backgroundColor="black"})
+    if(n >=1 && n <= 100){
+        for(let i = 0; i < n; i++){
+            for(let j = 0; j < n; j++){
+                let clonediv= div.cloneNode(true);
+                divs.appendChild(clonediv)
+                clonediv.style.cssText=(`width: calc(100%/${n});
+                height: calc(100%/${n})`)
+            }
+        }
+    }
+    else {errorMsg.innerText=('Value should be >= 100')}
 }
-}
-    usBtn.innerText=('Reset Page')
-    usBtn.addEventListener('click',()=>{location.reload()})
-}
+
+   
+
 
 usBtn.addEventListener('click', (value) => {
         value=usInput.value;
         gridSize(value)
+        usBtn.innerText=('Reset Page')
+        usBtn.addEventListener('click',()=>{location.reload()})
 })
